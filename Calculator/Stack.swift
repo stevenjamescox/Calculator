b378197270cf3c3f13ef4765a0ee4e1a53125040
Steve Cox <coxsteve@gmail.com>
2016-05-30 15:57:45 -0600

    setting up stack

---
+++ Calculator/Stack.swift
@@ -0,0 +1,41 @@
+//
+//  Stack.swift
+//  Calculator
+//
+//  Created by Steve Cox on 5/30/16.
+//  Copyright © 2016 DevMountain. All rights reserved.
+//
+
+import Foundation
+
+class Stack {
+    
+    
+    private var stack: [Float]
+    
+    init(stack: [Float]){
+    self.stack = stack
+    
+    }
+    
+    
+    func pop() -> Float {
+        
+        
+    }
+    
+    func push() -> Float {
+        
+    }
+    
+    func log(){
+        print(stack)
+    }
+    
+    func count() -> Int{
+        stack.count
+        
+    }
+    
+    
+}
