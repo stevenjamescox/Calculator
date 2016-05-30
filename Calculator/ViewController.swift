d1261f8e9f3972e755e4fe046b34a122d1716b42
Steve Cox <coxsteve@gmail.com>
2016-05-30 14:02:53 -0600

    set some colors eh

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UIViewController {
     
     let entryField = UILabel()
     let zeroButton = UIButton()
+    let oneButton = UIButton()
     let twoButton = UIButton()
     let threeButton = UIButton()
     let fourButton = UIButton()
@@ -25,10 +26,44 @@ class ViewController: UIViewController {
     let sevenButton = UIButton()
     let eightButton = UIButton()
     let nineButton = UIButton()
-    let clearButton = UIButton()
+    let enterButton = UIButton()
     let plusButton = UIButton()
     let minusButton = UIButton()
     let multiplyButton = UIButton()
     let divideButton = UIButton()
+    
+    
+    
+    func setupButtons() {
+        //set colors
+        entryField.backgroundColor = .blackColor()
+        zeroButton.backgroundColor = .grayColor()
+        oneButton.backgroundColor = .grayColor()
+        twoButton.backgroundColor = .grayColor()
+        threeButton.backgroundColor = .grayColor()
+        fourButton.backgroundColor = .grayColor()
+        fiveButton.backgroundColor = .grayColor()
+        sixButton.backgroundColor = .grayColor()
+        sevenButton.backgroundColor = .grayColor()
+        eightButton.backgroundColor = .grayColor()
+        nineButton.backgroundColor = .grayColor()
+        enterButton.backgroundColor = .purpleColor()
+        plusButton.backgroundColor = .orangeColor()
+        minusButton.backgroundColor = .orangeColor()
+        multiplyButton.backgroundColor = .orangeColor()
+        divideButton.backgroundColor = .orangeColor()
+        
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
