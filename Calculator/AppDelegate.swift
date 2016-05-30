2caa4edbe2a06cae3777633e385511f9156e30eb
Steve Cox <coxsteve@gmail.com>
2016-05-30 13:55:56 -0600

    yay UI Buttons programmaticallll

--- Calculator/AppDelegate.swift
+++ Calculator/AppDelegate.swift
@@ -13,9 +13,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-    
+        window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        window?.rootViewController = ViewController()
+        window?.backgroundColor = .blueColor()
+        window?.makeKeyAndVisible()
         return true
     }
 
--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -12,11 +12,23 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    let entryField = UILabel()
+    let zeroButton = UIButton()
+    let twoButton = UIButton()
+    let threeButton = UIButton()
+    let fourButton = UIButton()
+    let fiveButton = UIButton()
+    let sixButton = UIButton()
+    let sevenButton = UIButton()
+    let eightButton = UIButton()
+    let nineButton = UIButton()
+    let clearButton = UIButton()
+    let plusButton = UIButton()
+    let minusButton = UIButton()
+    let multiplyButton = UIButton()
+    let divideButton = UIButton()
+
 }
