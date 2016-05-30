ae84085ca608063d9347950174d7b9f859c140dd
Steve Cox <coxsteve@gmail.com>
2016-05-30 14:13:23 -0600

    setting up entryField constraints, & autoresizeno!

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    
+        setupButtons()
     }
     
     let entryField = UILabel()
@@ -35,8 +35,9 @@ class ViewController: UIViewController {
     
     
     func setupButtons() {
-        //set colors
+        //set colors & add buttons
         entryField.backgroundColor = .blackColor()
+        view.addSubview(entryField)
         zeroButton.backgroundColor = .grayColor()
         oneButton.backgroundColor = .grayColor()
         twoButton.backgroundColor = .grayColor()
@@ -53,10 +54,64 @@ class ViewController: UIViewController {
         multiplyButton.backgroundColor = .orangeColor()
         divideButton.backgroundColor = .orangeColor()
         
+        
+        //add buttons to view
+        view.addSubview(entryField)
+        view.addSubview(zeroButton)
+        view.addSubview(oneButton)
+        view.addSubview(twoButton)
+        view.addSubview(threeButton)
+        view.addSubview(fourButton)
+        view.addSubview(fiveButton)
+        view.addSubview(sixButton)
+        view.addSubview(sevenButton)
+        view.addSubview(eightButton)
+        view.addSubview(nineButton)
+        view.addSubview(enterButton)
+        view.addSubview(plusButton)
+        view.addSubview(minusButton)
+        view.addSubview(multiplyButton)
+        view.addSubview(divideButton)
+        
+}
+    
+    
+    func setupContraints() {
+        
+        // Turn off translatesAutoresizingMaskIntoConstraints YEAH
+        
+        entryField.translatesAutoresizingMaskIntoConstraints = false
+        zeroButton.translatesAutoresizingMaskIntoConstraints = false
+        oneButton.translatesAutoresizingMaskIntoConstraints = false
+        twoButton.translatesAutoresizingMaskIntoConstraints = false
+        threeButton.translatesAutoresizingMaskIntoConstraints = false
+        fourButton.translatesAutoresizingMaskIntoConstraints = false
+        fiveButton.translatesAutoresizingMaskIntoConstraints = false
+        sixButton.translatesAutoresizingMaskIntoConstraints = false
+        sevenButton.translatesAutoresizingMaskIntoConstraints = false
+        eightButton.translatesAutoresizingMaskIntoConstraints = false
+        nineButton.translatesAutoresizingMaskIntoConstraints = false
+        enterButton.translatesAutoresizingMaskIntoConstraints = false
+        plusButton.translatesAutoresizingMaskIntoConstraints = false
+        minusButton.translatesAutoresizingMaskIntoConstraints = false
+        multiplyButton.translatesAutoresizingMaskIntoConstraints = false
+        divideButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        // MARK: entryField size and constraints
+        
+        let entryFieldWidth = NSLayoutConstraint(item: entryField, attribute: .Width, relatedBy: .Equal, toItem: view, attribute: .Width, multiplier: 1.0, constant: 0)
+        
+        let entryFieldTopConstraint = NSLayoutConstraint(item: entryField, attribute: .Top, relatedBy: .Equal, toItem: view, attribute: .Top, multiplier: 1.0, constant: 0)
+        
+        let entryFieldLeadingConstraint = NSLayoutConstraint(item: entryField, attribute: .Leading, relatedBy: .Equal, toItem: view, attribute: .Leading, multiplier: 1.0, constant: 0)
+        
+        let entryFieldTrailingConstraint = NSLayoutConstraint(item: entryField, attribute: .Trailing, relatedBy: .Equal, toItem: view, attribute: .Leading, multiplier: 1.0, constant: 0)
+        
+        //
+    
+
 
 }
-
-
 
 
 
